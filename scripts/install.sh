06a90a9b50a8520e9b77fb1e742cc1603c04623c
MagnuSiwy <pankacper890@gmail.com>
2024-03-05 16:13:11 +0100

    Installation instructions changed

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
-sudo pacman -S --needed picom pacman-contrib rofi dunst kitty dolphin ark polkit-kde-agent pavucontrol nm-connection-editor bluez bluez-utils alsa-utils flameshot calcurse firefox kvantum qt5ct python-iwlib ttf-roboto otf-font-awesome
+sudo pacman -S --needed picom pacman-contrib rofi dunst kitty dolphin ark polkit-kde-agent pavucontrol nm-connection-editor bluez bluez-utils alsa-utils python-psutil flameshot calcurse firefox kvantum qt5ct python-iwlib ttf-roboto otf-font-awesome
 
-yay -S qtile-extras-git nwg-look tela-icon-theme bibata-cursor-theme catppuccin-gtk-theme-mocha blueman-git handlr-regex-bin xdg-utils-handlr
+yay -S nwg-look tela-icon-theme bibata-cursor-theme catppuccin-gtk-theme-mocha blueman-git handlr-regex xdg-utils-handlr
 
 echo "Most of the work is done. The only thing to do now is to copy somewhere the repo with catppuccin kvantum theme and set it in the kvantum app"
