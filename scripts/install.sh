0d5eabb529437629479ea1393d8d3241753dda62
MagnuSiwy <pankacper890@gmail.com>
2024-03-05 21:43:28 +0100

    Modified instalation scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-sudo pacman -S --needed picom pacman-contrib rofi dunst kitty dolphin ark polkit-kde-agent pavucontrol nm-connection-editor bluez bluez-utils alsa-utils python-psutil flameshot calcurse firefox kvantum qt5ct python-iwlib ttf-roboto otf-font-awesome
+sudo pacman -S --needed picom pacman-contrib rofi dunst kitty dolphin ark polkit-kde-agent pavucontrol nm-connection-editor bluez bluez-utils alsa-utils python-psutil numlockx flameshot calcurse firefox kvantum qt5ct python-iwlib ttf-roboto otf-font-awesome
 
 yay -S nwg-look tela-icon-theme bibata-cursor-theme catppuccin-gtk-theme-mocha blueman-git handlr-regex xdg-utils-handlr
 
